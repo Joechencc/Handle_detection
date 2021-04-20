0b7f1cee45b98a921e0e039162325e2e9756b7f0
Chao Chen <cchen@gsuite.ihmc.us>
2021-04-20 16:20:01 -0400

    visualization works

--- yolov5/detect_ros.py
+++ yolov5/detect_ros.py
@@ -57,9 +57,7 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
 
     
 def detect(image, depth):
-
     #cv_image = CvBridge().imgmsg_to_cv2(image, desired_encoding='bgr8')
-    print("hahah1")
     #print("image.shape:::::::::::::::::::::::::::::::::::::::"+str(np.array(image_np).shape))
     source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
@@ -101,20 +99,22 @@ def detect(image, depth):
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
     cudnn.benchmark = True  # set True to speed up constant image size inference
-    print("source"+str(source))
-    
-    
+    # print("source:::::"+str(source))
+      
+    #color_arr = np.frombuffer(image.data, np.uint8)
+    #image_np = cv2.imdecode(color_arr, cv2.IMREAD_COLOR)
+    #im0s = np.expand_dims(image_np, axis=0)
     color_arr = CvBridge().imgmsg_to_cv2(image, desired_encoding='bgr8')
     im0s = np.expand_dims(color_arr, axis=0)
     img0 = im0s.copy()
     
-    #depth_arr = CvBridge().imgmsg_to_cv2(depth, desired_encoding='16UC1')
+    depth_arr = CvBridge().imgmsg_to_cv2(depth, desired_encoding='16UC1')
     #depth_arr = np.frombuffer(depth.data, np.uint8)
     #print("depth_arr::::::::::"+str(np.asarray(depth_arr)))
     #depth_np = cv2.imdecode(depth_arr, cv2.IMREAD_GRAYSCALE)
 
-    #im1s = np.expand_dims(depth_arr, axis=0)
-    #img1 = im1s.copy()
+    im1s = np.expand_dims(depth_arr, axis=0) # Depth Frame
+    img1 = im1s.copy()
     
     # Letterbox
     s = np.stack([letterbox(x, new_shape=imgsz)[0].shape for x in im0s], 0)  # inference shapes
@@ -149,65 +149,70 @@ def detect(image, depth):
     # Apply Classifier
     if classify:
         pred = apply_classifier(pred, modelc, img, im0s)
+        print("hehehehehes")
 
         # Process detections
         # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
-            else:
-                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+    for i, det in enumerate(pred):  # detections per image
+        #if webcam:  # batch_size >= 1
+        s, im0 = '%g: ' % i, im0s[i].copy()
+        im1ss = im1s.copy()
+        #print("im1:::::::::::"+str(im1.shape))
+        #im1 = cv2.convertScaleAbs(im1, alpha=0.03)
+        #else:
+        #   p, s, im0, frame = path, '', im0s, getattr(dataset_color, 'frame', 0)
 
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+        #p = Path(p)  # to Path
+        #save_path = str(save_dir / p.name)  # img.jpg
+        #txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset_color.mode == 'image' else f'_{frame}')  # img.txt
+        s += '%gx%g ' % img.shape[2:]  # print string
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+            # Write results
+            for *xyxy, conf, cls in reversed(det):
+                if save_txt:  # Write to file
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                    with open(txt_path + '.txt', 'a') as f:
+                        f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
-                    if save_img or view_img:  # Add bbox to image
-                        label = f'{names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                if save_img or view_img:  # Add bbox to image
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
+        # Print time (inference + NMS)
+        print(f'{s}Done. ({t2 - t1:.3f}s)')
 
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1) # 1 millisecond
+        # Stream results
+        if view_img:
+            window_name = 'image'
+            cv2.imshow(window_name, im0)
+            cv2.waitKey(1) # 1 millisecond
 
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video'
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
+        # Save results (image with detections)
+        if save_img:
+            if dataset.mode == 'image':
+                cv2.imwrite(save_path, im0)
+            else:  # 'video'
+                if vid_path != save_path:  # new video
+                    vid_path = save_path
+                    if isinstance(vid_writer, cv2.VideoWriter):
+                        vid_writer.release()  # release previous video writer
 
-                        fourcc = 'mp4v'  # output video codec
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-                    vid_writer.write(im0)
+                    fourcc = 'mp4v'  # output video codec
+                    fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                    w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                    h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+                vid_writer.write(im0)
 
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
